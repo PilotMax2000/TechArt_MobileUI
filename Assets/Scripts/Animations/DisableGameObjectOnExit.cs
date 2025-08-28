ba30acec8033059143228054d29891d092b6e695
Roman Maksymenko <maksimenko.roma@gmail.com>
2025-08-28 22:38:41 +0200

    Added baase and settings popup prefabs, added animations for it

---
+++ Assets/Scripts/Animations/DisableGameObjectOnExit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace TechArtProject.Animations
+{
+    public class DisableGameObjectOnExit : StateMachineBehaviour
+    {
+        public bool DisableOnEnter = false;
+        public bool DisableOnExit = true;
+
+        // Called when we enter the state
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (DisableOnEnter)
+                animator.gameObject.SetActive(false);
+        }
+
+        // Called when we exit the state
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (DisableOnExit)
+                animator.gameObject.SetActive(false);
+        }
+    }
+}
