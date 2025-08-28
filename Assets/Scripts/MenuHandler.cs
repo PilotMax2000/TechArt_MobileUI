e1173377ec6e21d65a7192563c85eecb908e1715
Roman Maksymenko <maksimenko.roma@gmail.com>
2025-08-28 23:25:13 +0200

    Improved SettingsPopup animation

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace TechArtProject
 {
     public class MenuHandler : MonoBehaviour
     {
-        
+        [Header("Options Popup")]
+        [SerializeField] private PopupBackgroundBlur _popupBackgroundBlur;
+        [SerializeField] private Button _settingsPopupOpenButton;
+        [SerializeField] private Button _settingsPopupCloseButton;
+        [SerializeField] private GameObject _settingsPopup;
+        [SerializeField] private Animator _setttionsPopupAnimator;
+        [SerializeField] private string _settingsPopupCloseTrigger;
+        private int _cachedSettingsPopupCloseTrigger;
+
+        private void Awake()
+        {
+            _settingsPopupOpenButton.onClick.AddListener(OpenSettingsPopup);
+            _settingsPopupCloseButton.onClick.AddListener(CloseSettingsPopup);
+
+            _cachedSettingsPopupCloseTrigger = Animator.StringToHash(_settingsPopupCloseTrigger);
+        }
+
+        private void OpenSettingsPopup()
+        {
+            _popupBackgroundBlur.ApplyBlur();
+            _settingsPopup.gameObject.SetActive(true);
+        }
+
+        private void CloseSettingsPopup()
+        {
+            _setttionsPopupAnimator.SetTrigger(_cachedSettingsPopupCloseTrigger);
+        }
     }
 }
